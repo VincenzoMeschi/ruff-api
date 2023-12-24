fe2138ac2c7c3f8611b7d28b2e8fa0751c902744
VINCENZO MESCHI <vincenzomeschi12@gmail.com>
2023-12-24 17:47:13 -0500

    Prep for deployment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 import authRoute from "./routes/auth.js";
 import userRoute from "./routes/users.js";
 import movieRoute from "./routes/movies.js";
@@ -29,6 +30,18 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
 app.use("/api/lists", listRoute);
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.use("/admin", express.static(path.join(__dirname, "../admin/build")));
+
+// The catchall handler: for any request that doesn't match
+// one above, send back React's index.html file.
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+app.get("/admin/*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../admin/build/index.html"));
+});
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
